1e95a991f7de9216ed13b34f721d7d8527238177
Eduardo <graziolieduardo@gmail.com>
2021-10-27 17:21:23 +0200

    Add header scaffolding

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,62 @@
 <header>
     <div class="wrapper">
         <div id="logo">
-
+            <img src="{{ asset('images/dc-logo.png') }}" alt="">
         </div>
         <ul>
-
+            <li>
+                <a href="#">
+                    characters
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    comics
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    movies
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    tv
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    games
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    collectibles
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    videos
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    fans
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    news
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    shop
+                </a>
+            </li>
         </ul>
         <div class="search">
-
+            <input type="text">
         </div>
     </div>
 </header>
